633d411b3a38c5a4d88464aea23236a8be19e816
Phatchara Sangkeaw <phatchara.s@wjames.co>
2025-09-19 19:38:28 +0700

    Refactor Prisma configuration and update AppModule imports to include Prisma and Ticket modules. Change default port in main.ts from 3000 to 3001.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    TicketModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
